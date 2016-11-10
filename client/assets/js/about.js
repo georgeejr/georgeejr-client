72eb3a30eea295738b3ce166356080ca1710bfff
George Luis Elluranggo Jr <gelluranggo@corp.nextdigital.com>
2016-11-10 17:59:09 +0800

    update skills

--- client/assets/js/about.js
+++ client/assets/js/about.js
@@ -16,91 +16,105 @@
                         "current": 90,
                         "color":"#E44D26",
                         "bgcolor":"#eaeaea",
-                        "imgSrc":"http://img04.deviantart.net/7cad/i/2011/018/5/b/psd_of_html5_logo_by_tobrother-d37hlwk.png"
+                        "imgSrc":"assets/img/html5.png"
                     },
                     {
                         "name":"CSS3",
                         "current": 90,
                         "color":"#2262AF",
                         "bgcolor":"#eaeaea",
-                        "imgSrc":"http://www.virtualbank.co.zw/home/img/temp/css3.png"
+                        "imgSrc":"assets/img/css3.png"
                     },
                     {
                         "name":"SASS",
                         "current": 85,
                         "color":"#CC6699",
                         "bgcolor":"#eaeaea",
-                        "imgSrc":"https://s3-us-west-2.amazonaws.com/s.cdpn.io/74196/sass-logo.png"
+                        "imgSrc":"assets/img/sass.png"
                     },
                     {
                         "name":"Foundation",
                         "current": 80,
                         "color":"#4F91BF",
                         "bgcolor":"#eaeaea",
-                        "imgSrc":"http://www.jenkramer.org/img/yeti.png"
+                        "imgSrc":"assets/img/zurb.png"
                     },
                     {
                         "name":"Bootstrap",
                         "current": 85,
                         "color":"#553C7B",
                         "bgcolor":"#eaeaea",
-                        "imgSrc":"http://www.idevlive.com/media/images/services/thumb/bootstrap-logo.png"
+                        "imgSrc":"assets/img/bootstrap.png"
                     },
                     {
                         "name":"jQUery",
                         "current": 85,
                         "color":"#0867AB",
                         "bgcolor":"#eaeaea",
-                        "imgSrc":"http://ausago.com/wp-content/uploads/2011/03/jquery-logo-320x320.png"
+                        "imgSrc":"assets/img/jquery.png"
                     },
                     {
                         "name":"AngularJs",
                         "current": 80,
                         "color":"#DD1B16",
                         "bgcolor":"#eaeaea",
-                        "imgSrc":"https://angularjs.org/img/ng-logo.png"
+                        "imgSrc":"assets/img/angular.png"
                     },
                     {
                         "name":"Ionic",
                         "current": 65,
                         "color":"#4E8EF7",
                         "bgcolor":"#eaeaea",
-                        "imgSrc":"http://www.c10nect.com/Ionic_Logo.png"
+                        "imgSrc":"assets/img/ionic.png"
                     },
                     {
                         "name":"gulp",
                         "current": 70,
                         "color":"#EB4A4B",
                         "bgcolor":"#eaeaea",
-                        "imgSrc":"https://raw.githubusercontent.com/gulpjs/artwork/master/gulp-2x.png"
+                        "imgSrc":"assets/img/gulp.png"
                     },
                     {
                         "name":"GIT",
                         "current": 70,
                         "color":"#F34F29",
                         "bgcolor":"#eaeaea",
-                        "imgSrc":"https://git-scm.com/images/logos/logomark-orange@2x.png"
+                        "imgSrc":"assets/img/git.png"
                     },
                     {
                         "name":"EmberJs",
                         "current": 60,
                         "color":"#E0492F",
                         "bgcolor":"#eaeaea",
-                        "imgSrc":"http://www.gravatar.com/avatar/0cf15665a9146ba852bf042b0652780a?s=200"
+                        "imgSrc":"assets/img/ember.png"
                     },
                     {
                         "name":"HandleBarJs",
                         "current": 65,
                         "color":"#423426",
                         "bgcolor":"#eaeaea",
-                        "imgSrc":"http://www.46cl.fr/content/etude-de-cas/icons/icon-handlebars.png"
+                        "imgSrc":"assets/img/handlebar.png"
                     },
                     {
                         "name":"PHP/MySql",
                         "current": 60,
                         "color":"#6B7EB7",
                         "bgcolor":"#eaeaea",
-                        "imgSrc":"http://www.scottrobertswebdesign.com/wp-content/uploads/2014/07/php-mysql-logo.png"
+                        "imgSrc":"assets/img/php-mysql.png"
+                    },
+                    {
+                        "name":"Wordpress",
+                        "current": 75,
+                        "color":"#333333",
+                        "bgcolor":"#eaeaea",
+                        "imgSrc":"assets/img/wp.png"
+                    },
+                    {
+                        "name":"AEM",
+                        "current": 70,
+                        "color":"#FB7800",
+                        "bgcolor":"#eaeaea",
+                        "imgSrc":"assets/img/aem.png"
                     }
         ]
     }
